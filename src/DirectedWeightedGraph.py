2a7e7f67f48009b420799a16da06d03c2d05dbf6
Yehiel Siri <yehiels14@gmail.com>
2021-12-25 20:45:48 +0200

    DirectedWeightedGraph object part 2

--- src/DirectedWeightedGraph.py
+++ src/DirectedWeightedGraph.py
@@ -24,74 +24,154 @@ class DirectedWeightedGraph(ABC, GraphInterface):
 	Returns the number of vertices in this graph
 	@return: The number of vertices in this graph
 	"""
-	raise NotImplementedError
 
-    def e_size(self) -> int:
-        """
-        Returns the number of edges in this graph
-        @return: The number of edges in this graph
-        """
-        raise NotImplementedError
+	def e_size(self) -> int:
+		return self.edge_size
+	"""
+	Returns the number of edges in this graph
+	@return: The number of edges in this graph
+	"""
 
-    def get_all_v(self) -> dict:
-        """return a dictionary of all the nodes in the Graph, each node is represented using a pair
-         (node_id, node_data)
-        """
+	def get_all_v(self) -> dict:
+		return self.get_vertices()
 
-    def all_in_edges_of_node(self, id1: int) -> dict:
-        """return a dictionary of all the nodes connected to (into) node_id ,
-        each node is represented using a pair (other_node_id, weight)
-         """
+	"""
+	return a dictionary of all the nodes in the Graph, each node is represented using a pair (node_id, node_data)
+	"""
 
-    def all_out_edges_of_node(self, id1: int) -> dict:
-        """return a dictionary of all the nodes connected from node_id , each node is represented using a pair
-        (other_node_id, weight)
-        """
+	def all_in_edges_of_node(self, id1: int) -> dict:
+		if self.get_vertex(id1) is not None:
+			return self.get_vertex(id1).Ni_node_in
 
-    def get_mc(self) -> int:
-        """
-        Returns the current version of this graph,
-        on every change in the graph state - the MC should be increased
-        @return: The current version of this graph.
-        """
-        raise NotImplementedError
+	"""
+	return a dictionary of all the nodes connected to (into) node_id ,
+	each node is represented using a pair (other_node_id, weight)
+	"""
 
-    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-        """
-        Adds an edge to the graph.
-        @param id1: The start node of the edge
-        @param id2: The end node of the edge
-        @param weight: The weight of the edge
-        @return: True if the edge was added successfully, False o.w.
-        Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
-        """
-        raise NotImplementedError
+	def all_out_edges_of_node(self, id1: int) -> dict:
+		if self.get_vertex(id1) is not None:
+			return self.get_vertex(id1).Ni_node_out
 
-    def add_node(self, node_id: int, pos: tuple = None) -> bool:
-        """
-        Adds a node to the graph.
-        @param node_id: The node ID
-        @param pos: The position of the node
-        @return: True if the node was added successfully, False o.w.
-        Note: if the node id already exists the node will not be added
-        """
-        raise NotImplementedError
+	"""
+	return a dictionary of all the nodes connected from node_id , each node is represented using a pair
+	(other_node_id, weight)
+	"""
+
+	def get_mc(self) -> int:
+		return self.Mc
+	"""
+	Returns the current version of this graph,
+	on every change in the graph state - the MC should be increased
+	@return: The current version of this graph.
+	"""
+
+
+	def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+		if id2 in self.get_vertex(id1).Ni_node_out:
+			return False
+		if weight <= 0 or id1 == id2:
+			return False
+		boolAdd = False
+
+		if id1 not in self.DirectedWeightedGraph:
+			return boolAdd
+		if id2 not in self.DirectedWeightedGraph:
+			return boolAdd
+		
+		boolAdd = True
+
+		self.DirectedWeightedGraph[id1].add_out_Ni(id2, weight)
+		self.DirectedWeightedGraph[id2].add_in_Ni(id1, weight)
+
+		self.edge_size = self.edge_size + 1
+		self.Mc += 1
+		return boolAdd
+
+	"""
+	Adds an edge to the graph.
+	@param id1: The start node of the edge
+	@param id2: The end node of the edge
+	@param weight: The weight of the edge
+	@return: True if the edge was added successfully, False o.w.
+	Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
+	"""
+
+
+	def add_node(self, node_id: int, pos: tuple = None) -> bool:
+		x = False
+		if node_id in self.Graph_DW:
+			return x
+		x = True
+		self.num_vertices = self.num_vertices + 1
+		new_Node = Node(node_id, pos)
+		self.Mc = self.Mc + 1
+		self.Graph_DW[node_id] = new_Node
+
+		return x
+
+	"""
+	Adds a node to the graph.
+	@param node_id: The node ID
+	@param pos: The position of the node
+	@return: True if the node was added successfully, False o.w.
+	Note: if the node id already exists the node will not be added
+	"""
 
     def remove_node(self, node_id: int) -> bool:
-        """
-        Removes a node from the graph.
-        @param node_id: The node ID
-        @return: True if the node was removed successfully, False o.w.
-        Note: if the node id does not exists the function will do nothing
-        """
-        raise NotImplementedError
+		bool1 = False
+		if node_id not in self.Graph_DW:
+			return bool1
 
-    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-        """
-        Removes an edge from the graph.
-        @param node_id1: The start node of the edge
-        @param node_id2: The end node of the edge
-        @return: True if the edge was removed successfully, False o.w.
-        Note: If such an edge does not exists the function will do nothing
-        """
-        raise NotImplementedError
+		if len(self.get_vertex(node_id).Ni_node_in )> 0:
+			for node in self.Graph_DW[node_id].Ni_node_in.keys():
+				self.Graph_DW[node].Ni_node_out.pop(node_id)
+				self.edge_size -= 1
+
+		if len(self.get_vertex(node_id).Ni_node_out) > 0:
+			for node in self.Graph_DW[node_id].Ni_node_out.keys():
+				self.Graph_DW[node].Ni_node_in.pop(node_id)
+				self.edge_size -= 1
+
+		self.Mc += 1
+		self.num_vertices = self.num_vertices - 1
+		self.Graph_DW.pop(node_id)
+		return bool1
+
+	"""
+	Removes a node from the graph.
+	@param node_id: The node ID
+	@return: True if the node was removed successfully, False o.w.
+	Note: if the node id does not exists the function will do nothing
+	"""
+
+
+	def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+		boolRemove = False
+		if self.get_vertex(node_id1) is None or self.get_vertex(node_id2) is None or node_id1 == node_id2:
+			return boolRemove
+
+		if node_id1 not in self.Graph_DW:
+			return boolRemove
+
+		if node_id2 not in self.Graph_DW:
+			return boolRemove
+		if node_id1 not in self.get_vertex(node_id2).Ni_node_in or node_id2 not in self.get_vertex(node_id1).Ni_node_out:
+			return boolRemove
+		self.get_vertex(node_id2).deleteNi_in(node_id1)
+		self.get_vertex(node_id1).deleteNi_out(node_id2)
+
+		boolRemove = True
+
+		self.edge_size -= 1
+		self.Mc += 1
+		return boolRemove
+
+	"""
+	Removes an edge from the graph.
+	@param node_id1: The start node of the edge
+	@param node_id2: The end node of the edge
+	@return: True if the edge was removed successfully, False o.w.
+	Note: If such an edge does not exists the function will do nothing
+	"""
+
+
